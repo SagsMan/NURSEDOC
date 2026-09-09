1cd880e462dec9a0337e86a0f9bc5a5df3351124
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2026-09-09 16:21:59 +0100

    Add Technical Lead Co-founder profile page

---
+++ technical-lead-details.php
@@ -0,0 +1,39 @@
+<?php include ("header.php"); ?>
+
+        <section class="page-header">
+            <div class="page-header__bg"></div>
+            <div class="page-header__shape"></div>
+            <div class="container">
+                <h2 class="page-header__title bw-split-in-right">Technical Lead Co-founder</h2>
+                <ul class="careold-breadcrumb list-unstyled">
+                    <li><a href="index.php">Home</a></li>
+                    <li><span>Technical Lead Co-founder</span></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="team-details">
+            <div class="container">
+                <div class="row gutter-y-60 align-items-center">
+                    <div class="col-lg-5">
+                        <div class="team-details__image wow fadeInLeft" data-wow-delay="100ms">
+                            <img src="assets/images/team/sagiru-garba.jpg" alt="Sagiru Garba">
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="team-details__content wow fadeInUp" data-wow-delay="200ms">
+                            <h3 class="team-details__title">Sagiru Garba</h3>
+                            <div class="team-details__designation">Technical Lead Co-founder</div>
+                            <p class="team-details__text">
+                                Sagiru Garba is the Technical Lead Co-founder of NurseDoc Connect, helping shape the technology that supports accessible, connected healthcare across Nigeria.
+                            </p>
+                            <div class="team-details__highlight">
+                                <p class="team-details__highlight__phone"><i class="fas fa-phone-square-alt"></i><a href="contact.php">Connect with NurseDoc Connect</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+       <?php include("footer.php") ;?>
