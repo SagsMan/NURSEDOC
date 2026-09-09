fd73f7b000d770e85eb3c045f7bb422178c6e1aa
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2026-09-09 16:20:08 +0100

    Add Lead Health Educator profile page

---
+++ priscilla-robeson-details.php
@@ -0,0 +1,39 @@
+<?php include ("header.php"); ?>
+
+        <section class="page-header">
+            <div class="page-header__bg"></div>
+            <div class="page-header__shape"></div>
+            <div class="container">
+                <h2 class="page-header__title bw-split-in-right">Lead Health Educator</h2>
+                <ul class="careold-breadcrumb list-unstyled">
+                    <li><a href="index.php">Home</a></li>
+                    <li><span>Lead Health Educator</span></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="team-details">
+            <div class="container">
+                <div class="row gutter-y-60 align-items-center">
+                    <div class="col-lg-5">
+                        <div class="team-details__image wow fadeInLeft" data-wow-delay="100ms">
+                            <img src="assets/images/team/priscilla-robeson.jpg" alt="Priscilla Robeson">
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="team-details__content wow fadeInUp" data-wow-delay="200ms">
+                            <h3 class="team-details__title">Priscilla Robeson</h3>
+                            <div class="team-details__designation">Lead Health Educator &amp; Nursing Advisor</div>
+                            <p class="team-details__text">
+                                Priscilla Robeson is the Lead Health Educator and Nursing Advisor at NurseDoc Connect, supporting clear, practical health education and compassionate care guidance.
+                            </p>
+                            <div class="team-details__highlight">
+                                <p class="team-details__highlight__phone"><i class="fas fa-phone-square-alt"></i><a href="contact.php">Connect with NurseDoc Connect</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+       <?php include("footer.php") ;?>
